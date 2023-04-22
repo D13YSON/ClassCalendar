9c8954e7c78c0e19cfcc31692d2a3d4bdfac5315
D13YSON <dieysonjason@gmail.com>
2023-04-21 21:06:09 -0300

    R

---
+++ projeto-2/build/generated-sources/ap-source-output/model/Aluno_.java
@@ -0,0 +1,18 @@
+package model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-05-31T18:53:16", comments="EclipseLink-2.7.9.v20210604-rNA")
+@StaticMetamodel(Aluno.class)
+public class Aluno_ { 
+
+    public static volatile SingularAttribute<Aluno, String> telefone;
+    public static volatile SingularAttribute<Aluno, String> endereco;
+    public static volatile SingularAttribute<Aluno, String> cpf;
+    public static volatile SingularAttribute<Aluno, Integer> idAluno;
+    public static volatile SingularAttribute<Aluno, String> nome;
+    public static volatile SingularAttribute<Aluno, String> turma;
+
+}
